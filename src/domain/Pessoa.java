8eb84636516692c0196b5d937132f099d01a425f
Schelleges <ricardoschelleges@gotmail.com>
2020-06-11 17:48:17 -0300

    João Maria e Yago

---
+++ src/domain/Pessoa.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class Pessoa {
+	private String name;
+	private String Idade;
+	private String programa;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIdade() {
+		return Idade;
+	}
+
+	public void setIdade(String idade) {
+		Idade = idade;
+	}
+
+	public String getPrograma() {
+		return programa;
+	}
+
+	public void setPrograma(String programa) {
+		this.programa = programa;
+	}
+
+}
